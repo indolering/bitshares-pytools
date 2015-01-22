d248b009a96e16690e11895ea1d7b5dc00ae9ff7
Fabian Schuh <schuh@lnt.de>
2015-01-22 13:34:29 +0100

    more checks - btscheckdecode btscheckencode with ripmed160 for crc

--- bts-transaction.py
+++ bts-transaction.py
@@ -7,118 +7,19 @@ import sys
 import unittest
 import struct
 from binascii import hexlify
+from genbtskey import *
 
 enc_memo_hex = "" # enc with otk_private
 tx1_balance_id = "BTS5bJNzfPVQxEahXp28H85hnL9GvdbiHdPf"
-
-# https://github.com/gferrin/bitcoin-code/blob/master/utils.py
-b58 = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
-
-PREFIX = "BTS"
-
-def base58encode(n):
-    result = ''
-    while n > 0:
-        result = b58[n%58] + result
-        n /= 58
-    return result
-
-def base58decode(s):
-    result = 0
-    for i in range(0, len(s)):
-        result = result * 58 + b58.index(s[i])
-    return result
-
-def base256decode(s):
-    result = 0
-    for c in s:
-        result = result * 256 + ord(c)
-    return result
-
-def base256encode(n):
-    result = ''
-    while n > 0:
-        result = chr(n % 256) + result
-        n /= 256
-    return result
-
-def countLeadingChars(s, ch):
-    count = 0
-    for c in s:
-        if c == ch:
-            count += 1
-        else:
-            break
-    return count
-
-# https://en.bitcoin.it/wiki/Base58Check_encoding
-def base58CheckEncode(version, payload):
-    s = chr(version) + payload
-    checksum = hashlib.sha256(hashlib.sha256(s).digest()).digest()[0:4]
-    result = s + checksum
-    leadingZeros = countLeadingChars(result, '\0')
-    return '1' * leadingZeros + base58encode(base256decode(result))
-
-def base58CheckDecode(s):
-    leadingOnes = countLeadingChars(s, '1')
-    s = base256encode(base58decode(s))
-    result = '\0' * leadingOnes + s[:-4]
-    chk = s[-4:]
-    checksum = hashlib.sha256(hashlib.sha256(result).digest()).digest()[0:4]
-    assert(chk == checksum)
-    version = result[0]
-    return result[1:]
-
-def wifKeyToPrivateKey(s) :
-    return base58CheckDecode(s).encode('hex')
-
-def privateKeyToWif(key_hex):    
-    return base58CheckEncode(0x80, key_hex.decode('hex'))
-    
-def privateKeyToPublicKey(s):
-    sk = ecdsa.SigningKey.from_string(s.decode('hex'), curve=ecdsa.SECP256k1)
-    return ('\04' + sk.verifying_key.to_string()).encode('hex')
-
-def ripemd160(s):
-    ripemd160 = hashlib.new('ripemd160')
-    ripemd160.update(s)
-    return ripemd160.digest()
-    
-def compressedpubkey(s):
-    # https://github.com/lyndsysimon/cryptocoin/blob/master/cryptocoin/key.py 
-    point = ecdsa.SigningKey.from_string(s.decode('hex'), curve=ecdsa.SECP256k1).verifying_key.pubkey.point
-    x = hex(point.x())[2:].strip('L')
-    y = hex(point.y())[2:].strip('L')
-    return ''.join(( '02' if point.y() % 2 == 0 else '03', x))
-
-def keyToPubKey(s): # raw pubkey
-    btspubkey  = compressedpubkey(s).decode('hex')
-    return base58encode(base256decode(btspubkey))
-
-def keyToBTSPubKey(s):
-    btspubkey  = compressedpubkey(s).decode('hex')
-    myhash     = ripemd160(btspubkey)
-    return PREFIX + base58encode(base256decode(btspubkey + myhash[ :4 ]))
-
-def keyToBTSAddress(s) :
-    btspubkey  = compressedpubkey(s).decode('hex')
-    myaddress  = ripemd160(hashlib.sha512(btspubkey).digest())
-    myhash     = ripemd160(myaddress)
-    return PREFIX + base58encode(base256decode(myaddress + myhash[ :4 ]))
-
-def pubkeyToBTSAddress(s) :
-    btspubkey  = s.decode('hex')
-    myaddress  = ripemd160(hashlib.sha512(btspubkey).digest())
-    myhash     = ripemd160(myaddress)
-    return PREFIX + base58encode(base256decode(myaddress + myhash[ :4 ]))
 
 def trx( privkey, topubkey, amount, asset_id, balance_id ) :
     fee               = .5 * 1e5
 
     #####################
     # Withdraw condition for deposit ( equals 'output' )
-    owner                  = ripemd160(hashlib.sha512(topubkey).digest())
-    memo_otk               = ""
+    #owner                  = ripemd160(hashlib.sha512(topubkey).digest())
+    owner                  = btsbase58CheckDecode("XTS8QZgeFurRa6hzZVDtUDfvg7u7LAySeuNK")
+    memo_otk               = btsbase58CheckDecode("XTS54pK2P15bth1C4v9QLtMQDuV7vR8RQQttMBHMzScrJdG7GsBza")
     memo_data              = ""
     slate_id               = 0
     type_id                = 1 # withdraw_signature_type
@@ -128,17 +29,20 @@ def trx( privkey, topubkey, amount, asset_id, balance_id ) :
     values                 = ( asset_id, slate_id, type_id )
     wc                     = struct.pack("<iqb",*values) + withdraw_sig_condition
     print ("withdraw_condition: " + hexlify(wc))
-    deposit                = struct.pack("<q", amount) + wc # Deposit operation ( this will be the complete 'output' )
-    print ("deposit: " + hexlify(deposit))
 
-    #####################
-    # Withdraw
-    balanceAddressRaw = base256encode(base58decode(balance_id[ len( PREFIX ): ]))
-    balanceAddress    = balanceAddressRaw[ :20 ] ## fixme check checksum
-    claim_input_data  = ""
-    values            = ( balanceAddress, amount+fee, len(claim_input_data), claim_input_data)
-    withdraw          = struct.pack('<20sids',*values)
-    print (hexlify(withdraw))
+
+    print("soll: 1600000000000000000137513dbafc2b1e175d91e3d7d71181b9742fdf90dc01021772ca67d26ed39f30580bb7e2ac5952bba74de08db7246b19c7ad0ae439280e00")
+#     deposit                = struct.pack("<q", amount) + wc # Deposit operation ( this will be the complete 'output' )
+#     print ("deposit: " + hexlify(deposit))
+# 
+#     #####################
+#     # Withdraw
+#     balanceAddressRaw = base58decode(balance_id[ len( PREFIX ): ])
+#     balanceAddress    = balanceAddressRaw[ :20 ] ## fixme check checksum
+#     claim_input_data  = ""
+#     values            = ( balanceAddress, amount+fee, len(claim_input_data), claim_input_data)
+#     withdraw          = struct.pack('<20sids',*values)
+#     print (hexlify(withdraw))
 
 if __name__ == '__main__':
     private_key = os.urandom(32).encode('hex')
@@ -151,44 +55,24 @@ if __name__ == '__main__':
     print "BTS Address             : %s " % keyToBTSAddress(private_key)
     print "-"*80
 
-    print trx( private_key, keyToPubKey(private_key), 10*1e5, 0, tx1_balance_id )
+    print trx( private_key, "", 10*1e5, 0, tx1_balance_id )
 
 
-wc = bts.blockchain.WithdrawCondition.fromJson({                                                                                                                                                                                          
-"asset_id": 22,                                                                                                                                                                                       
-"slate_id": 0,                                                                                                                                                                                        
-"type": "withdraw_signature_type",                                                                                                                                                                    
-"data": {                                                                                                                                                                                             
-"owner": "XTS8QZgeFurRa6hzZVDtUDfvg7u7LAySeuNK",                                                                                                                                                    
-"memo": {                                                                                                                                                                                           
-"one_time_key": "XTS54pK2P15bth1C4v9QLtMQDuV7vR8RQQttMBHMzScrJdG7GsBza",                                                                                                                          
-"encrypted_memo_data": ""                                         
-} } })
-Object { asset_id: 22, slate_id: 0, type_id: 1, condition: Object }
-wc
-Object { asset_id: 22, slate_id: 0, type_id: 1, condition: Object }
-wc.toHex()
-"1600000000000000000177513dbafc2b1e175d91e3d7d71181b9742fdf90dc01021772ca67d26ed39f30580bb7e2ac5952bba74de08db7246b19c7ad0ae439280e404a2c983ebe4b5ee84cdf21f27c730d1aaf577e24e5a3649a1404cc521ed8644a760f53eb6936ffe68a7fea26a5a7970f71d4700d2eb8452c0dfe514f26948e21"
-wc.condition
-Object { owner: Uint8Array[20], one_time_key: Object, encrypted_memo: Uint8Array[0], type_name: "withdraw_signature_type", type_id: undefined }
-
-
-
-wc = bts.blockchain.WithdrawCondition.fromJson({                                                                                                                                                                                          
-"asset_id": 22,                                                                                                                                                                                       
-"slate_id": 0,                                                                                                                                                                                        
-"type": "withdraw_signature_type",                                                                                                                                                                    
-"data": {                                                                                                                                                                                             
-"owner": "XTS8QZgeFurRa6hzZVDtUDfvg7u7LAySeuNK",                                                                                                                                                    
-"memo": {                                                                                                                                                                                           
-"one_time_key": "XTS54pK2P15bth1C4v9QLtMQDuV7vR8RQQttMBHMzScrJdG7GsBza",                                                                                                                          
-"encrypted_memo_data": ""                                         
-} } })
-Object { asset_id: 22, slate_id: 0, type_id: 1, condition: Object }
-wc.toHex()
-"1600000000000000000137513dbafc2b1e175d91e3d7d71181b9742fdf90dc01021772ca67d26ed39f30580bb7e2ac5952bba74de08db7246b19c7ad0ae439280e00"
-
-wc.condition
-Object { owner: Uint8Array[20], one_time_key: Object, encrypted_memo: Uint8Array[0], type_name: "withdraw_signature_type", type_id: undefined }
-
-
+# wc = bts.blockchain.WithdrawCondition.fromJson({                                                                                                                                                                                          
+# "asset_id": 22,                                                                                                                                                                                       
+# "slate_id": 0,                                                                                                                                                                                        
+# "type": "withdraw_signature_type",                                                                                                                                                                    
+# "data": {                                                                                                                                                                                             
+# "owner": "XTS8QZgeFurRa6hzZVDtUDfvg7u7LAySeuNK",                                                                                                                                                    
+# "memo": {                                                                                                                                                                                           
+# "one_time_key": "XTS54pK2P15bth1C4v9QLtMQDuV7vR8RQQttMBHMzScrJdG7GsBza",                                                                                                                          
+# "encrypted_memo_data": ""                                         
+# } } })
+# Object { asset_id: 22, slate_id: 0, type_id: 1, condition: Object }
+# wc.toHex()
+# "1600000000000000000137513dbafc2b1e175d91e3d7d71181b9742fdf90dc01021772ca67d26ed39f30580bb7e2ac5952bba74de08db7246b19c7ad0ae439280e00"
+# 
+# wc.condition
+# Object { owner: Uint8Array[20], one_time_key: Object, encrypted_memo: Uint8Array[0], type_name: "withdraw_signature_type", type_id: undefined }
+# 
+# # 
--- genbtskey.py
+++ genbtskey.py
@@ -6,6 +6,9 @@ import os
 import sys
 import unittest
 
+PREFIX = "BTS"
+
+##############################################
 # https://github.com/kylegibson/python-bitcoin-client/blob/master/base58.py
 __b58chars = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
 __b58base = len(__b58chars)
@@ -43,6 +46,7 @@ def base58decode(v):
     result = chr(0)*nPad + result
     return result
 
+##############################################
 # https://en.bitcoin.it/wiki/Base58Check_encoding
 def base58CheckEncode(version, payload):
     s = chr(version) + payload
@@ -56,7 +60,17 @@ def base58CheckDecode(s):
     checksum = hashlib.sha256(hashlib.sha256(dec).digest()).digest()[0:4]
     assert(s[-4:] == checksum)
     return dec[1:]
+##############################################
+def btsbase58CheckEncode(s):
+    return base58encode(s + ripemd160(s)[ :4 ])
 
+def btsbase58CheckDecode(s):
+    s   = base58decode(s)
+    dec = s[:-4]
+    checksum = ripemd160(dec)[0:4]
+    assert(s[-4:] == checksum)
+    return dec[1:]
+##############################################
 def wifKeyToPrivateKey(s) :
     return base58CheckDecode(s).encode('hex')
 
@@ -87,23 +101,19 @@ def compressedpubkey(s):
 
 def keyToBTSPubKey(s):
     btspubkey  = compressedpubkey(s).decode('hex')
-    myhash     = ripemd160(btspubkey)
-    return "BTS" + base58encode(btspubkey + myhash[ :4 ])
+    return PREFIX + btsbase58CheckEncode(btspubkey)
 
 def keyToBTSAddress(s) :
     btspubkey  = compressedpubkey(s).decode('hex')
     myaddress  = ripemd160(hashlib.sha512(btspubkey).digest())
-    myhash     = ripemd160(myaddress)
-    return "BTS" + base58encode(myaddress + myhash[ :4 ])
+    return PREFIX + btsbase58CheckEncode(myaddress)
 
 def btcPubKeyToBTSAddress(btspubkey) :
     myaddress  = ripemd160(hashlib.sha512(btspubkey.decode( 'hex' )).digest())
-    myhash     = ripemd160(myaddress)
-    return "BTS" + base58encode(myaddress + myhash[ :4 ])
+    return PREFIX + btsbase58CheckEncode(myaddress)
 
 def btcPubkeytoBTSpubkey(btcpubkey) :
-    myhash     = ripemd160(btcpubkey.decode( 'hex' ))
-    return "BTS" + base58encode(btcpubkey.decode( 'hex' ) + myhash[ :4 ])
+    return PREFIX + btsbase58CheckEncode(btcpubkey.decode('hex'))
 
 class Testcases(unittest.TestCase) :
     def test_btspubkey(self):
@@ -142,6 +152,31 @@ class Testcases(unittest.TestCase) :
         self.assertEqual("5HvVz6XMx84aC5KaaBbwYrRLvWE46cH6zVnv4827SBPLorg76oq",privateKeyToWif("0e1bfc9024d1f55a7855dc690e45b2e089d2d825a4671a3c3c7e4ea4e74ec00e"))
         self.assertEqual("5Jete5oFNjjk3aUMkKuxgAXsp7ZyhgJbYNiNjHLvq5xzXkiqw7R",privateKeyToWif("6e5cc4653d46e690c709ed9e0570a2c75a286ad7c1bc69a648aae6855d919d3e"))
         self.assertEqual("5KDT58ksNsVKjYShG4Ls5ZtredybSxzmKec8juj7CojZj6LPRF7",privateKeyToWif("b84abd64d66ee1dd614230ebbe9d9c6d66d78d93927c395196666762e9ad69d8"))
+
+    def test_btsb58(self):
+        ###### Fails due to missing 02 at the beginning ##
+        self.assertEqual("5ywapm5TuAfnj2FquQvwH8tXEGdzzpTvyNVyq298RB1XnFn6oK",btsbase58CheckEncode("0290137a9c8189754dffb3c7641ad76faa61a0c8fe977c291e44be47d1d9082c9a".decode('hex')))
+        ##################################################
+        self.assertEqual("6dumtt9swxCqwdPZBGXh9YmHoEjFFnNfwHaTqRbQTghGAY2gRz",btsbase58CheckEncode("02e649f63f8e8121345fd7f47d0d185a3ccaa843115cd2e9392dcd9b82263bc680".decode('hex')))
+        self.assertEqual("5725vivYpuFWbeyTifZ5KevnHyqXCi5hwHbNU9cYz1FHbFXCxX",btsbase58CheckEncode("021c7359cd885c0e319924d97e3980206ad64387aff54908241125b3a88b55ca16".decode('hex')))
+        self.assertEqual("6kZKHSuxqAwdCYsMvwTcipoTsNE2jmEUNBQufGYywpniBKXWZK",btsbase58CheckEncode("02f561e0b57a552df3fa1df2d87a906b7a9fc33a83d5d15fa68a644ecb0806b49a".decode('hex')))
+        self.assertEqual("8b82mpnH8YX1E9RHnU2a2YgLTZ8ooevEGP9N15c1yFqhoBvJur",btsbase58CheckEncode("03e7595c3e6b58f907bee951dc29796f3757307e700ecf3d09307a0cc4a564eba3".decode('hex')))
+
+    def test_btsb58rev(self):
+        ###### Fails due to missing 02 at the beginning ##
+        self.assertEqual(btsbase58CheckDecode("5ywapm5TuAfnj2FquQvwH8tXEGdzzpTvyNVyq298RB1XnFn6oK"),"0290137a9c8189754dffb3c7641ad76faa61a0c8fe977c291e44be47d1d9082c9a".decode('hex'))
+        ##################################################
+        self.assertEqual(btsbase58CheckDecode("6dumtt9swxCqwdPZBGXh9YmHoEjFFnNfwHaTqRbQTghGAY2gRz"),"02e649f63f8e8121345fd7f47d0d185a3ccaa843115cd2e9392dcd9b82263bc680".decode('hex'))
+        self.assertEqual(btsbase58CheckDecode("5725vivYpuFWbeyTifZ5KevnHyqXCi5hwHbNU9cYz1FHbFXCxX"),"021c7359cd885c0e319924d97e3980206ad64387aff54908241125b3a88b55ca16".decode('hex'))
+        self.assertEqual(btsbase58CheckDecode("6kZKHSuxqAwdCYsMvwTcipoTsNE2jmEUNBQufGYywpniBKXWZK"),"02f561e0b57a552df3fa1df2d87a906b7a9fc33a83d5d15fa68a644ecb0806b49a".decode('hex'))
+        self.assertEqual(btsbase58CheckDecode("8b82mpnH8YX1E9RHnU2a2YgLTZ8ooevEGP9N15c1yFqhoBvJur"),"03e7595c3e6b58f907bee951dc29796f3757307e700ecf3d09307a0cc4a564eba3".decode('hex'))
+
+    def test_btsb58(self):
+        self.assertEqual("0290137a9c8189754dffb3c7641ad76faa61a0c8fe977c291e44be47d1d9082c9a",btsbase58CheckDecode(btsbase58CheckEncode("0290137a9c8189754dffb3c7641ad76faa61a0c8fe977c291e44be47d1d9082c9a".decode('hex'))).encode('hex'))
+        self.assertEqual("02e649f63f8e8121345fd7f47d0d185a3ccaa843115cd2e9392dcd9b82263bc680",btsbase58CheckDecode(btsbase58CheckEncode("02e649f63f8e8121345fd7f47d0d185a3ccaa843115cd2e9392dcd9b82263bc680".decode('hex'))).encode('hex'))
+        self.assertEqual("021c7359cd885c0e319924d97e3980206ad64387aff54908241125b3a88b55ca16",btsbase58CheckDecode(btsbase58CheckEncode("021c7359cd885c0e319924d97e3980206ad64387aff54908241125b3a88b55ca16".decode('hex'))).encode('hex'))
+        self.assertEqual("02f561e0b57a552df3fa1df2d87a906b7a9fc33a83d5d15fa68a644ecb0806b49a",btsbase58CheckDecode(btsbase58CheckEncode("02f561e0b57a552df3fa1df2d87a906b7a9fc33a83d5d15fa68a644ecb0806b49a".decode('hex'))).encode('hex'))
+        self.assertEqual("03e7595c3e6b58f907bee951dc29796f3757307e700ecf3d09307a0cc4a564eba3",btsbase58CheckDecode(btsbase58CheckEncode("03e7595c3e6b58f907bee951dc29796f3757307e700ecf3d09307a0cc4a564eba3".decode('hex'))).encode('hex'))
 
 if __name__ == '__main__':
     # unittest.main()
